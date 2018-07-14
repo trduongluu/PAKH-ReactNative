aea522498663123e813f64e607c9621ed5619755
Trieu Duong <duonggoku@gmail.com>
2018-07-15 06:58:04 +0700

    tab bar done

--- App.js
+++ App.js
@@ -4,10 +4,19 @@ import Screen1 from "./tabs/screen1"
 import Screen2 from "./tabs/screen2"
 import Screen3 from "./tabs/screen3"
 
+console.disableYellowBox = true;
+
 var myTabs = TabNavigator({
     Tab1 : {screen:Screen1},
     Tab2 : {screen:Screen2},
     Tab3 : {screen:Screen3}
-});
+},
+{
+    tabBarPosition: 'bottom',
+    animationEnabled: true,
+    tabBarOptions: {
+      activeTintColor: 'red',
+    }
+  });
 
 export default myTabs;
