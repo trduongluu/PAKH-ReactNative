99e4c264d1d32889c49ac6197339b054d8a813a4
trduong <duonggoku@gmail.com>
2018-07-20 12:20:11 +0700

    Commit 1

--- App.js
+++ App.js
@@ -1,22 +1,128 @@
-import {TabNavigator} from "react-navigation";
+import React from 'react';
+import {StyleSheet} from 'react-native'
+import {createBottomTabNavigator, createStackNavigator} from "react-navigation";
+import Icon from 'react-native-vector-icons/Ionicons';
 
-import Screen1 from "./tabs/screen1"
-import Screen2 from "./tabs/screen2"
-import Screen3 from "./tabs/screen3"
+import Receive from "./src/ReceiveRQ/Receive";
+import Send from "./src/SendRQ/Send";
+import Search from "./src/Search/Search";
+import Setting from "./src/Setting/Setting";
 
 console.disableYellowBox = true;
 
-var myTabs = TabNavigator({
-    Tab1 : {screen:Screen1},
-    Tab2 : {screen:Screen2},
-    Tab3 : {screen:Screen3}
-},
-{
-    tabBarPosition: 'bottom',
-    animationEnabled: true,
-    tabBarOptions: {
-      activeTintColor: 'red',
-    }
-  });
+export default class App extends React.Component{
+  render() {
+    return(
+      <BottomTabNav/>
+    );
+  }
+}
 
-export default myTabs;
+const ReceiveStack = createStackNavigator({
+  Receive: {
+    screen: Receive
+  }
+}, {
+  navigationOptions: {
+    headerTitle: 'Received',
+    headerTitleStyle: {
+      textAlign: 'center',
+      alignSelf: 'center'
+    }
+  }
+});
+
+const SendStack = createStackNavigator({
+  Send: {
+    screen: Send,
+    navigationOptions: {
+      title: 'Sent'
+    }
+  }
+});
+
+const SearchStack = createStackNavigator({
+  Search: {
+    screen: Search,
+    navigationOptions: {
+      title: 'Search'
+    }
+  }
+});
+
+const SettingStack = createStackNavigator({
+  Setting: {
+    screen: Setting,
+    navigationOptions: {
+      title: 'Setting'
+    }
+  }
+});
+
+const BottomTabNav = createBottomTabNavigator({
+  Receive: {
+    screen: ReceiveStack,
+    navigationOptions: {
+      tabBarLabel: 'Receive',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="ios-book" color={tintColor} size={24} />
+      )
+    }
+  },
+  Send: {
+    screen: SendStack,
+    navigationOptions: {
+      tabBarLabel: 'Send',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="ios-send" color={tintColor} size={24} />
+      )
+    }
+  },
+  Search: {
+    screen: SearchStack,
+    navigationOptions: {
+      tabBarLabel: 'Search',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="ios-search" color={tintColor} size={24} />
+      )
+    }
+  },
+  Setting: {
+    screen: SettingStack,
+    navigationOptions: {
+      tabBarLabel: 'Setting',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="ios-settings" color={tintColor} size={24} />
+      )
+    }
+  }
+}, {
+    initialRouteName: 'Receive',
+    order: ['Receive', 'Send', 'Search', 'Setting'],
+    navigationOptions: {
+      tabBarVisible: true
+    },
+    tabBarOptions: {
+      activeTintColor: 'blue',
+      inactiveTintColor: 'grey'
+    }
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
